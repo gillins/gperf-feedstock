91b0aef2c16e78b1274e6545ab0a8e9aac270cb6
Sam Gillingham <gillingham.sam@gmail.com>
2020-11-26 13:30:13 +1000

    progress on recipe

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,13 @@
 
-bash -c "configure --host=i686-w64-mingw32 --prefix=${CYGWIN_PREFIX}/Library ^
-    CC='${RECIPE_DIR}/build-aux/compile cl -nologo' ^
-    CFLAGS='-MD' LD='link' NM='dumpbin -symbols' STRIP=':' ^
-    AR='${RECIPE_DIR}/build-aux/ar-lib lib' RANLIB=':'"
+FOR /F "delims=" %%i IN ('cygpath -u -p "%cd%"') DO set "PWD=%%i"
+
+bash -c "configure --host=i686-w64-mingw32 --prefix=%CYGWIN_PREFIX%/Library CC='%PWD%/build-aux/compile cl -nologo' CFLAGS='-MD' LD='link' NM='dumpbin -symbols' STRIP=':' AR='%PWD%/build-aux/ar-lib lib' RANLIB=':'"
 if errorlevel 1 exit 1
 
 make
 if errorlevel 1 exit 1
 make check
 if errorlevel 1 exit 1
-
+make install
+exit 1
+if errorlevel 1 exit 1
